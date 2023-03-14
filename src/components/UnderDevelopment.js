03719133365b8246a60ac7c4ba4378ada7f7d2ad
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-03-14 17:53:30 -0400

    added underdevelopment screen

---
+++ src/components/UnderDevelopment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UnderDevelopment = () => {
+  return (
+    <div className="under-development">
+      <h1>Under Development</h1>
+      <p>
+        The mobile version of this website is currently under development.
+        Please visit us from a larger screen.
+      </p>
+    </div>
+  );
+};
+
+export default UnderDevelopment;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./state/store";
+import UnderDevelopment from "./components/UnderDevelopment";
 
 const root = createRoot(document.querySelector("#root"));
 
 root.render(
   <Provider store={store}>
+    <UnderDevelopment />
     <App />
   </Provider>
 );
